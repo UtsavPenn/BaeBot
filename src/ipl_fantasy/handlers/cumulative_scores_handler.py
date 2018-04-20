90f6a54cd95dc54dab3f4fc93e5321d5c9590fe9
Utsav Mehta <utsav@docasap.com>
2018-04-20 15:27:06 -0400

    temp fix for cumulative scores.

--- src/ipl_fantasy/handlers/cumulative_scores_handler.py
+++ src/ipl_fantasy/handlers/cumulative_scores_handler.py
@@ -1,10 +1,13 @@
 from ipl_fantasy.common import get_league_team_name_for_user, USER_IDS, simple_table
 from ipl_fantasy.data import get_live_score_for_user
 
-
+def get_total_score_so_far_for_user(user_id):
+    for member in get_league_details()['leagueMembers']:
+        if member['userId'] == user_id:
+            return member['totalPoints']
 
 def get_live_total_scores():
-    live_total_scores = [(get_league_team_name_for_user(user), get_live_score_for_user(user)+get_total_score_so_far_for_user(user)) for user in USER_IDS]        
+    live_total_scores = [(get_league_team_name_for_user(user), get_total_score_so_far_for_user(user)) for user in USER_IDS]        
     return sorted(live_total_scores, key=lambda x: int(x[1]), reverse=True)
 
 def cumulative_scores(bot, update):
