5921e2df7c9a42a8f795491529fc252c32f45308
Reddy <tarunreddy.bethi@gmail.com>
2018-05-08 17:17:46 -0700

    Pretty printing players

--- bae_bot/ipl_fantasy/handlers/top_picks_handler.py
+++ bae_bot/ipl_fantasy/handlers/top_picks_handler.py
@@ -15,8 +15,9 @@ def get_all_players_rank_data():
         batting_rank = player_standing['battingRank']
         bowling_rank = player_standing['bowlingRank']
         team_name = get_player(player_id)['team']
+        player_type = get_player(player_id).type
         player_points = sum((v for k,v in player_standing.items() if k.endswith('P')))
-        player_info.append((player_id,player_name,batting_rank,bowling_rank,player_points,team_name))
+        player_info.append((player_id,player_name,player_type, batting_rank,bowling_rank,player_points,team_name))
     #write_updated_top_picks_to_file(json.dumps(player_info))
     #write_updated_top_picks_to_file(data)
     return player_info
@@ -25,8 +26,8 @@ def get_top_picks(short_team_name=None):
     all_players = get_all_players_rank_data()
     if short_team_name:
         team_name = determine_team(short_team_name)
-        all_players = [x for x in all_players if x[5] == team_name]
-    top_players =  sorted(all_players, key=lambda x: int(x[4]), reverse=True)[:5]
+        all_players = [x for x in all_players if x[6] == team_name]
+    top_players = sorted(all_players, key=lambda x: int(x[5]), reverse=True)[:5]
     return top_players
 
 def top_picks(bot, update, args): 
@@ -39,6 +40,6 @@ def top_picks(bot, update, args):
     rankings_table = []
 
     for rank in rankings:
-        row = [rank[1],rank[4]]
+        row = [rank[1],rank[5], rank[2]]
         rankings_table.append(row)
     bot.send_message(update.message.chat_id,simple_table(rankings_table))
--- tests/ipl_fantasy/test_common.py
+++ tests/ipl_fantasy/test_common.py
@@ -3,4 +3,6 @@ from bae_bot.ipl_fantasy.common import determine_player
 
 def test_determine_player():
     player = determine_player('thamp')
-    assert player.name.startswith('Basil Thampi (sh)')
+    assert player.name.startswith('Basil')
+    assert player.name.endswith('(sh)')
+    assert player.team == "sunrisers-hyderabad"
