7943728bcfe383603e448f3e2daaac7d3689e319
Reddy <tarunreddy.bethi@gmail.com>
2018-06-03 20:04:20 -0400

    pick -> place

--- bae_bot/fifawc/handlers/pick_handler.py
+++ bae_bot/fifawc/handlers/pick_handler.py
@@ -7,7 +7,7 @@ def pick(bot, update, args):
     user = update.effective_user.first_name
     
     if len(args) < 3 or not args :
-      bot.send_message(update.message.chat_id, "Usage: /pick 1234 Argentina 10")
+      bot.send_message(update.message.chat_id, "Usage: /pick 1234 ARG 10")
       return
     bet = BetsHistory(event_id=args[0], user_id=user, bet_amount=args[2],result=args[1],bet_processed=0)
     bet.save()
